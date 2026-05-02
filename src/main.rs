a5cac87ce1b1f2ee3f595275475a0481389ecdda
Suranjan Das <das@primalwoke.com>
2026-05-02 19:12:04 +0530

    removed ability to edit contracts (frontend js only)

--- src/main.rs
+++ src/main.rs
@@ -163,7 +163,7 @@ async fn main() {
 async fn handler() -> IndexTemplate {
     IndexTemplate {
         title: "Invoices&Contracts v0.1".to_string(),
-        message: "Invoice&Contract v0.1".to_string(),
+        message: "Invoices&Contracts v0.1".to_string(),
     }
 }
 
